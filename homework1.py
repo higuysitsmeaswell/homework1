c625e1362d03a2d6ba0ee583661435383a2f4476
LightningL <corymj2014@gmail.com>
2026-02-18 17:02:34 +0000

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,75 @@
+import pygame,sys
+import os
+pygame.font.init()
+pygame.init()
+WIDTH = 900
+HEIGHT = 500
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.update()
+pygame.display.set_caption("Space invaders")
+WHITE = (255,255,255)
+RED = (255,0,0)
+YELLOW = (255,255,0)
+Border = pygame.Rect(WIDTH//2 - 5,0,10,HEIGHT)
+HEALTHFONT = pygame.font.SysFont("aeriel",40)
+WINNERFONT = pygame.font.SysFont("aeriel",100)
+BULLET_VEL = 7 
+MAX_BULLETS = 3
+YELLOW_HIT = pygame.USEREVENT + 1
+RED_HIT = pygame.USEREVENT + 2
+FPS = 60
+VEL = 3
+SPACESHIP_WIDTH = 50
+SPACESHIP_HEIGHT = 40
+YELLOW_IMAGE = pygame.image.load(os.path.join("Assets","PlayerRocketyellow.png"))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_IMAGE,(SPACESHIP_WIDTH,SPACESHIP_HEIGHT)),90)
+SPACE_IMAGE = pygame.image.load(os.path.join("Assets","Space.png"))
+SPACE = pygame.transform.scale(SPACE_IMAGE,(WIDTH,HEIGHT))
+RED_IMAGE = pygame.image.load(os.path.join("Assets","PlayerRocketred.png"))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_IMAGE,(SPACESHIP_WIDTH,SPACESHIP_HEIGHT)),90)
+RED_HEALTH = 10
+YELLOW_HEALTH = 10
+def draw_window(RED_HEALTH,YELLOW_HEALTH,RED_BULLETS,YELLOW_BULLETS,RED,YELLOW):
+    WIN.blit(SPACE,(0,0))
+    RED_HEALTH_TEXT = HEALTHFONT.render("Health: "+str(RED_HEALTH),1,WHITE)
+    YELLOW_HEALTH_TEXT = HEALTHFONT.render("Health: "+str(YELLOW_HEALTH),1,WHITE)
+    WIN.blit(RED_HEALTH_TEXT,(WIDTH-RED_HEALTH_TEXT.get_width()-10,10))
+    WIN.blit(YELLOW_HEALTH_TEXT,(WIDTH-YELLOW_HEALTH_TEXT.get_width()-10,10))
+
+    WIN.blit(YELLOW_SPACESHIP,(YELLOW.x,YELLOW.y))
+    WIN.blit(RED_SPACESHIP,(RED.x,RED.y))
+
+    for bullet in RED_BULLETS:
+        pygame.draw.rect(WIN,RED,bullet)
+    for bullet in YELLOW_BULLETS:
+        pygame.draw.rect(WIN,YELLOW,bullet)
+    pygame.display.update()
+
+
+def yellow_move(keys_pressed,YELLOW):
+    if keys_pressed[pygame.K_a] and YELLOW.x - VEL > 0:
+        YELLOW.x -= VEL
+    if keys_pressed[pygame.K_d] and YELLOW.x + VEL + YELLOW.width < WIDTH:
+        YELLOW.x += VEL
+    if keys_pressed[pygame.K_w] and YELLOW.y - VEL > 0:
+        YELLOW.y -= VEL
+    if keys_pressed[pygame.K_s] and YELLOW.y + VEL + YELLOW.height < HEIGHT:
+        YELLOW.y += VEL
+
+def red_move(keys_pressed, RED):
+    if keys_pressed[pygame.K_a] and RED.x - VEL > 0:
+        RED.x -= VEL
+    if keys_pressed[pygame.K_d] and RED.x + VEL + RED.width < WIDTH:
+        RED.x += VEL
+    if keys_pressed[pygame.K_w] and RED.y - VEL > 0:
+        RED.y -= VEL
+    if keys_pressed[pygame.K_s] and RED.y + VEL + RED.height < HEIGHT:
+        RED.y += VEL
+
+pygame.display.update()
+while True:
+    draw_window()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    
